41187f477eea310c647e9c91e97b76387431238d
Timothy Barna <audioclimb@gmail.com>
2020-03-02 14:01:33 -0700

    working

--- server/getProducts.js
+++ server/getProducts.js
@@ -2,10 +2,11 @@ const products = require('../products.json');
 
 const getProducts = (req, res) => {
     const item = products.find(val => val.id === parseInt(req.params.id));
-    if (!item) {
-        return res.status(500).send('Item not in list');
+    if (req.query.price) {
+        const items = products.filter(val => val.price >= parseInt(req.query.price));
+        return res.status(200).send(items);
     }
-    res.status(200).send(item);
+    res.status(200).send(products);
 }
 
 module.exports = getProducts;
