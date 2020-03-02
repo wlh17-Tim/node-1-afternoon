587193e480bb1a869a7adf577378c941ed6fac37
Timothy Barna <audioclimb@gmail.com>
2020-03-02 12:28:13 -0700

    working

--- server/getProducts.js
+++ server/getProducts.js
@@ -1,7 +1,11 @@
 const products = require('../products.json');
 
 const getProducts = (req, res) => {
-    res.status(200).send(products);
+    const item = products.find(val => val.id === parseInt(req.params.id));
+    if (!item) {
+        return res.status(500).send('Item not in list');
+    }
+    res.status(200).send(item);
 }
 
 module.exports = getProducts;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const products = require('../products.json');
+const getProducts = require('./getProducts');
+const getProduct = require('./getProduct');
 
 const app = express();
-const getProducts = require('./getProducts');
 
 const port = 3333;
 
 app.get('/api/products', getProducts);
+app.get('/api/product/:id', getProduct);
 
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}`);
